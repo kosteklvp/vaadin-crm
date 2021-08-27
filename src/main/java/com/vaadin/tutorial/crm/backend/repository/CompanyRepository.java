de59473ea700274d73a7977db02f2c0f08528be9
Piotr Kostański <kostek.lvp@gmail.com>
2021-08-28 00:22:50 +0200

    1

---
+++ src/main/java/com/vaadin/tutorial/crm/backend/repository/CompanyRepository.java
@@ -0,0 +1,8 @@
+package com.vaadin.tutorial.crm.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.vaadin.tutorial.crm.backend.entity.Company;
+
+public interface CompanyRepository extends JpaRepository<Company, Long> {
+}
